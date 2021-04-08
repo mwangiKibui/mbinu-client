2168841c805c5e4c594931e19f8b1773ab5546a9
Kennedy Mwangi <47610365+mwangiKibui@users.noreply.github.com>
2021-04-08 14:16:48 +0300

    fix:modifying server config

--- server.js
+++ server.js
@@ -14,9 +14,7 @@ app.prepare().then(() => {
     const server = express();
         // Static files
         // https://github.com/zeit/next.js/tree/4.2.3#user-content-static-file-serving-eg-images
-    server.use('/images', express.static(path.join(__dirname, 'images'), {
-        maxAge: dev ? '0' : '365d'
-    }));
+    app.use(express.static(path.join(__dirname,'public')));
 
     server.use(bodyParser.json());
 
